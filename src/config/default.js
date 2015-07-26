fe1b8632c004745936d617b678576ad0e542ca44
Albert Fernández <albertfdp@gmail.com>
2015-07-26 13:07:32 +0200

    Use env variables here

--- src/config/default.js
+++ src/config/default.js
@@ -10,10 +10,10 @@ module.exports = {
     port: 3001
   },
   podio: {
-    clientId: '',
-    clientSecret: '',
-    username: '',
-    password: '',
+    clientId: process.env.CLIENT_ID || '',
+    clientSecret: process.env.CLIENT_SECRET || '',
+    username: process.env.USERNAME || '',
+    password: process.env.PASSWORD || '',
     apps: {
       todo: {
         app_id: '',
